ad1152ba0d32aa1f4269d0f2b55de3b18e19a2cb
Caio Bianchi <caiohbs23@gmail.com>
2023-10-04 16:25:28 -0300

    added docstring for serializers

--- backend/helenite_app/serializers.py
+++ backend/helenite_app/serializers.py
@@ -7,8 +7,16 @@ from helenite_app.models import Profile, Post, Like, Comment
 
 class ProfileSerializer(serializers.ModelSerializer):
     """
-    TODO: Add documentation.
+    This serializer represents a Profile.
+
+    Fields:
+        - user_pk: the ID of the user associated with the profile (read-only);
+        - username: the username of the user associated with the profile (read-only);
+        - get_full_name: the full associated with the profile;
+        - endpoint: the profile's endpoint;
+        - pfp: the URL of the user's profile picture.
     """
+
     user_pk = serializers.CharField(source="user.pk", read_only=True)
     username = serializers.CharField(source="user.username", read_only=True)
 
@@ -25,8 +33,19 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 class FeedSerializer(serializers.ModelSerializer):
     """
-    TODO: Add documentation.
+    This serializer is responsible for providing the posts for the feed.
+
+    Fields:
+        - profile: utilizes the `ProfileSerializer` to provide info related to the user;
+        - post_publication_date: publication date for post;
+        - endpoint: the endpoint for the post;
+        - post_text: the text associated with the post;
+        - post_image: the image associated with the post;
+        - likes_count: the amount of likes on the post;
+        - comments_count: the amount of comments on the post;
+        - likes: the usernames of the users who liked the post.
     """
+
     likes = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
     comments_count = serializers.SerializerMethodField()
@@ -60,8 +79,20 @@ class FeedSerializer(serializers.ModelSerializer):
 
 class FeedWithoutProfileInfoSerializer(FeedSerializer):
     """
-    TODO: Add documentation.
+    This serializer inherits from `FeedSerializer` and provides the same info,
+    but with no profile data they will already be on the post. Used exclusively
+    on `FeedForSingleProfileSerializer`.
+
+    Fields:
+        - post_publication_date: publication date for post;
+        - endpoint: the endpoint for the post;
+        - post_text: the text associated with the post;
+        - post_image: the image associated with the post;
+        - likes_count: the amount of likes on the post;
+        - comments_count: the amount of comments on the post;
+        - likes: the usernames of the users who liked the post.
     """
+
     class Meta:
         model = Post
         fields = [
@@ -77,8 +108,15 @@ class FeedWithoutProfileInfoSerializer(FeedSerializer):
 
 class FeedForSingleProfileSerializer(ProfileSerializer):
     """
-    TODO: Add documentation.
+    This serializer is responsible for providing the feed for the single profile
+    view, retrieving both profile info, as well as posts made by the user.
+
+    Fields:
+        - profile: the profile info;
+        - posts: the posts made by the user through `FeedWithoutProfileInfoSerializer` (ready_only);
+        - birthday: shows the birthday should the user allow on settings.
     """
+
     posts = FeedWithoutProfileInfoSerializer(
         many=True, source="user.created_posts", read_only=True
     )
@@ -99,8 +137,15 @@ class FeedForSingleProfileSerializer(ProfileSerializer):
 
 class SinglePostSerializer(FeedSerializer):
     """
-    TODO: Add documentation.
+    This serializer is responsible for providing information for a single profile,
+    retrieving both profile info for original poster, as well as comments and likes.
+
+    Fields:
+        - profile: the posts made by the user through `ProfileSerializer`;
+        - post: the post itself through `FeedSerializer`;
+        - comments: the comments left on the post.
     """
+
     comments = serializers.SerializerMethodField()
 
     class Meta:
@@ -114,9 +159,9 @@ class SinglePostSerializer(FeedSerializer):
             for comment in comments
         ]
         return comment_data
-    
 
-class NewCommentSerializer():
+
+class NewCommentSerializer:
     # TODO: This.
     """
     TODO: Add documentation.
@@ -126,8 +171,14 @@ class NewCommentSerializer():
 
 class NewPostSerializer(serializers.ModelSerializer):
     """
-    TODO: Add documentation.
+    This serializer is responsible for a way to make a new post.
+
+    Fields:
+        - parent_user: the user making the post;
+        - post_text: the text for the post (not-required so long as there's as image);
+        - post_image: the image associated with the post (not-required so long as there's text).
     """
+
     parent_user = serializers.CharField(source="post_parent_user", read_only=True)
 
     class Meta:
@@ -141,11 +192,24 @@ class NewPostSerializer(serializers.ModelSerializer):
 
 class SettingsSerializer(serializers.ModelSerializer):
     """
-    TODO: Add documentation.
+    This serializer is responsible for providing/changing settings associated with
+    an account, also allowing for password change and pfp change.
+
+    Fields:
+        - pfp: the profile picture for the account;
+        - private_profile: private_profile (boolean);
+        - show_birthday: show_birthday setting (boolean);
+        - old_password: the old password for the account (write-only);
+        - new_password: the new password (write-only);
+        - confirm_new_password: confirmation for new password (write-only).
     """
+
     old_password = serializers.CharField(write_only=True, required=False)
     new_password = serializers.CharField(write_only=True, required=False)
-    confirm_new_password = serializers.CharField(write_only=True, required=False,)
+    confirm_new_password = serializers.CharField(
+        write_only=True,
+        required=False,
+    )
 
     class Meta:
         model = Profile
@@ -159,13 +223,17 @@ class SettingsSerializer(serializers.ModelSerializer):
         ]
 
     def validate(self, data):
-        if data.get('new_password'):
-            if data.get('confirm_new_password') == None:
-                raise serializers.ValidationError("Confirmation password wasn't provided")
-            elif data.get('old_password') == None:
+        if data.get("new_password"):
+            if data.get("confirm_new_password") == None:
+                raise serializers.ValidationError(
+                    "Confirmation password wasn't provided"
+                )
+            elif data.get("old_password") == None:
                 raise serializers.ValidationError("Old password wasn't provided")
-            elif data.get('new_password') != data.get('confirm_new_password'):
-                raise serializers.ValidationError("Unable to confirm new password. Are they the same?")
+            elif data.get("new_password") != data.get("confirm_new_password"):
+                raise serializers.ValidationError(
+                    "Unable to confirm new password. Are they the same?"
+                )
             else:
                 # TODO: Add password verification.
                 pass
