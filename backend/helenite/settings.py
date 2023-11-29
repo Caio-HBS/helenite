9b99e0337d58e8c9d73d9fe2bd652edab4dd5300
Caio Bianchi <caiohbs23@gmail.com>
2023-11-29 11:57:39 -0300

    added new packages

--- backend/helenite/settings.py
+++ backend/helenite/settings.py
@@ -25,24 +25,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    'corsheaders',
     # Django apps
     'helenite_app',
     # Django REST Framework
     'rest_framework',
     'rest_framework.authtoken',
-    # Third party API services
+    # Third-party API services
     'algoliasearch_django',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'helenite.urls'
 
