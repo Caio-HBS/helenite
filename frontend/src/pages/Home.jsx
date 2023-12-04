69e8f3ec36d5b7c2b7a63e31b40f8f4fa6531464
Caio Bianchi <caiohbs23@gmail.com>
2023-12-04 12:01:26 -0300

    progress on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,31 +1,45 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 
 import ParticlesComponent from "../components/Particles.jsx";
 
 export default function HomePage() {
-  // TODO: outsource most of this to a separate component?
+  const navigate = useNavigate();
+
+  const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/login");
+    }
+  }, []);
+
   const buttonClass =
-    "bg-helenite-light-blue hover:bg-helenite-dark-blue rounded-md p-2";
+    "bg-helenite-light-blue hover:bg-helenite-dark-blue hover:text-helenite-white rounded-md p-2";
 
   return (
     <>
       <div className="pt-16">
         <ParticlesComponent />
-
         <div
           id="main-element"
           className="flex flex-col h-screen items-center justify-center"
         >
-          <div className="text-center p-10 rounded-md bg-helenite-dark-grey shadow-2xl text-helenite-white flex relative">
+          <div>
+            <p className="text-3xl m-2 text-helenite-light-blue relative">
+              <strong>Welcome to Helenite!</strong>
+            </p>
+          </div>
+          <div className="text-center p-10 rounded-md bg-helenite-dark-grey shadow-2xl  flex relative">
             <div className="flex flex-col">
-              <label>Already have an account?</label>
+              <label className="text-white">Already have an account?</label>
               <Link to="/login">
                 <button className={buttonClass}>Log in</button>
               </Link>
             </div>
             <div className="ml-4 flex flex-col">
-              <label>Doesn't have an account?</label>
+              <label className="text-white">Doesn't have an account?</label>
               <Link to="/register">
                 <button className={buttonClass}>Sign up</button>
               </Link>
