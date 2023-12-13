90c5cb3ed4e00da239b5bb236b033ae418fc1cfd
Caio Bianchi <caiohbs23@gmail.com>
2023-12-13 10:12:49 -0300

    added new page

---
+++ frontend/src/pages/Friends.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default function FriendsPage() {
+  return (
+    <>
+      <Helmet>
+        <title>Helenite | Friends</title>
+      </Helmet>
+      <div>
+        <div className="pt-16 bg-stone-800">
+          // TODO: friends page.
+          <div>Friends page</div>
+        </div>
+      </div>
+    </>
+  );
+}
