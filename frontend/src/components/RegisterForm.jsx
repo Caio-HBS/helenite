01720cfc3c4d5bffa52944a4414efedc386b0f9e
Caio Bianchi <caiohbs23@gmail.com>
2023-12-18 10:40:25 -0300

    finished registration form

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,27 +1,188 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import {
+  isBirthplace,
+  isDate,
+  isEmail,
+  isSlug,
+  isValidName,
+  isValidPassword,
+} from "../utils/validation.js";
+const backendURL = import.meta.env.VITE_REACT_BACKEND_URL;
 
 import LoadingButton from "./LoadingButton.jsx";
 
 export default function RegisterForm() {
   const [showPassword, setShowPassowrd] = useState(false);
+  const [sendingRequest, setSendingRequest] = useState(false);
   const [validation, setValidation] = useState(true);
   const [errorMsg, setErrorMsg] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [profileSlug, setProfileSlug] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [showBirthday, setShowBirthday] = useState(true);
+  const [birthPlace, setBirthPlace] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [privateProfile, setpPivateProfile] = useState(false);
 
-  function showPasswordHandler() {
-    setShowPassowrd((oldState) => !oldState);
-  }
+  const navigate = useNavigate();
 
   const inputClass =
     "w-full py-1 focus:outline-none focus:border-0 focus:outline-helenite-light-blue";
 
   const divClass = "py-1";
 
-  return (
-    // TODO: add functionality.
-    // TODO: implement validarotors.
+  function showPasswordHandler() {
+    setShowPassowrd((oldState) => !oldState);
+  }
 
+  async function handleRegistration(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    const user = username;
+    if (user !== "" && isValidName(user)) {
+      formData.append("username", user);
+    } else {
+      setValidation(false);
+      setErrorMsg("Username is not valid.");
+      return;
+    }
+
+    const userEmail = email;
+    if (userEmail !== "" && isEmail(userEmail)) {
+      formData.append("email", userEmail);
+    } else {
+      setValidation(false);
+      setErrorMsg("Email is not valid.");
+      return;
+    }
+
+    const slug = profileSlug;
+    if (slug !== "" && isSlug(slug)) {
+      formData.append("custom_slug_profile", slug);
+    } else {
+      setValidation(false);
+      setErrorMsg("Custom path to profile is not valid.");
+      return;
+    }
+
+    const fName = firstName;
+    const lName = lastName;
+    if (
+      fName !== "" &&
+      lName !== "" &&
+      isValidName(fName) &&
+      isValidName(fName)
+    ) {
+      formData.append("first_name", fName);
+      formData.append("last_name", lName);
+    } else {
+      setValidation(false);
+      setErrorMsg("First or last name not valid.");
+      return;
+    }
+
+    const date = birthday;
+    if (date !== "" && isDate(date)) {
+      formData.append("birthday", date);
+    } else {
+      setValidation(false);
+      setErrorMsg("Birthday is not valid.");
+      return;
+    }
+
+    const showBirth = showBirthday;
+    formData.append("show_birthday", showBirth);
+
+    const place = birthPlace;
+    if (place !== "" && isBirthplace(place)) {
+      formData.append("birth_place", birthPlace);
+    } else {
+      setValidation(false);
+      setErrorMsg("Not a valid country.");
+      return;
+    }
+
+    const pass = password;
+    const confirmPass = confirmPassword;
+    if (pass !== "" && confirmPass !== "") {
+      if (
+        pass === confirmPass &&
+        isValidPassword(pass) &&
+        isValidPassword(confirmPass)
+      ) {
+        formData.append("password", pass);
+        formData.append("confirmation_password", confirmPass);
+      } else {
+        setValidation(false);
+        setErrorMsg("Passwords don't match.");
+        return;
+      }
+    } else {
+      setValidation(false);
+      setErrorMsg("Please choose valid passwords.");
+      return;
+    }
+
+    const privProfile = privateProfile;
+    formData.append("private_profile", privProfile);
+
+    setSendingRequest(true);
+
+    const response = await fetch(`${backendURL}/api/v1/register/`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      const resError = await response.json();
+      toast.error(resError.message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      setSendingRequest(false);
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    toast.success(resData.detail, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+    setSendingRequest(false);
+    const delayNavigate = setTimeout(() => {
+      clearTimeout(delayNavigate);
+      navigate("/login");
+    }, 5000);
+  }
+
+  return (
     <>
-      <form className="flex flex-col h-screen items-center justify-center relative">
+      <ToastContainer />
+      <form
+        onSubmit={(event) => handleRegistration(event)}
+        className="flex flex-col h-screen items-center justify-center relative"
+      >
         <div className="text-left p-10 rounded-md bg-helenite-dark-grey shadow-md shadow-stone-900">
           <div>
             <p className="text-2xl text-center pb-1 text-helenite-light-blue">
@@ -33,6 +194,8 @@ export default function RegisterForm() {
               type="text"
               name="username"
               placeholder="Your username"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
               className={inputClass}
               required
             />
@@ -42,6 +205,8 @@ export default function RegisterForm() {
               type="email"
               name="email"
               placeholder="Your email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
               className={inputClass}
               required
             />
@@ -51,6 +216,8 @@ export default function RegisterForm() {
               type="slug"
               name="custom_slug_profile"
               placeholder="Path for your profile (blank for username)"
+              value={profileSlug}
+              onChange={(event) => setProfileSlug(event.target.value)}
               className={inputClass}
               required
             />
@@ -60,6 +227,8 @@ export default function RegisterForm() {
               type="text"
               name="first_name"
               placeholder="Your First Name"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
               className={inputClass}
               required
             />
@@ -69,6 +238,8 @@ export default function RegisterForm() {
               type="text"
               name="last_name"
               placeholder="Your Last Name"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
               className={inputClass}
               required
             />
@@ -77,6 +248,8 @@ export default function RegisterForm() {
             <input
               type="date"
               name="birthday"
+              value={birthday}
+              onChange={(event) => setBirthday(event.target.value)}
               className={inputClass}
               required
             />
@@ -84,10 +257,11 @@ export default function RegisterForm() {
           <div className={divClass}>
             <input
               type="checkbox"
-              name="show_birthday"
+              id="show_birthday"
+              onChange={(event) => setShowBirthday(event.target.value)}
               className="py-1 focus:outline-none focus:border-0 focus:outline-helenite-light-blue"
             />
-            <label className="pl-2 text-helenite-white">
+            <label htmlFor="show_birthday" className="pl-2 text-helenite-white">
               Make my birthday public
             </label>
           </div>
@@ -95,7 +269,9 @@ export default function RegisterForm() {
             <input
               type="text"
               name="birth_place"
-              placeholder="Where were you born?"
+              placeholder="Where were you born? (Country)"
+              value={birthPlace}
+              onChange={(event) => setBirthPlace(event.target.value)}
               className={inputClass}
               required
             />
@@ -106,6 +282,8 @@ export default function RegisterForm() {
               name="password"
               minLength={8}
               placeholder="Your password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
               className={inputClass}
               required
             />
@@ -116,6 +294,8 @@ export default function RegisterForm() {
               name="confirmation_password"
               minLength={8}
               placeholder="Confirm your password"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
               className={inputClass}
               required
             />
@@ -130,10 +310,14 @@ export default function RegisterForm() {
           <div className={divClass}>
             <input
               type="checkbox"
-              name="private_profile"
+              id="private_profile"
+              onChange={(event) => setpPivateProfile(event.target.value)}
               className="py-1 focus:outline-none focus:border-0 focus:outline-helenite-light-blue"
             />
-            <label className="pl-2 text-helenite-white">
+            <label
+              htmlFor="private_profile"
+              className="pl-2 text-helenite-white"
+            >
               Make my profile private
             </label>
           </div>
@@ -143,7 +327,7 @@ export default function RegisterForm() {
             </p>
           )}
           <div className="pt-2 text-center">
-            {showPassword && (
+            {sendingRequest && (
               <LoadingButton
                 buttonText="Loading..."
                 textColor="text-helenite-white"
@@ -151,7 +335,7 @@ export default function RegisterForm() {
                 padding="px-5 py-2.5 text-lg"
               />
             )}
-            {!showPassword && (
+            {!sendingRequest && (
               <button className="p-2 text-xl rounded-lg bg-helenite-light-blue hover:bg-helenite-dark-blue hover:text-helenite-white">
                 Take me to Helenite!
               </button>
